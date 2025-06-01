186248a4523b67658cffccb8ff57344a0be0629d
Peter Fuhrmann <tixiv@gmx.net>
2025-06-01 11:18:51 +0200

    Add copy files script

---
+++ copy_files.sh
@@ -0,0 +1,5 @@
+cp -r Assemblies /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/EasyCaravanAndGo/
+cp -r About /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/EasyCaravanAndGo/
+cp -r Languages /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/EasyCaravanAndGo/
+cp -r Textures /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/EasyCaravanAndGo/
+
